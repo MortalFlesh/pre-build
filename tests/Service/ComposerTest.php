49934d111bd6c2b41f87c1f40d279e117f17826d
Petr Chromec <mortal_flesh@seznam.cz>
2020-02-24 00:29:11 +0100

    Fix version in composer json

---
+++ tests/Service/ComposerTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace MF\PreBuild\Service;
+
+use PHPUnit\Framework\TestCase;
+use function safe\file_get_contents;
+
+class ComposerTest extends TestCase
+{
+    public function testShouldFindVersionInComposerJson(): void
+    {
+        $composer = json_decode(file_get_contents(__DIR__ . '/../../composer.json'), true);
+
+        $this->assertArrayHasKey(
+            'version',
+            $composer,
+            'Since the version is used inside application and the console, it must remain here.'
+        );
+    }
+}
